09784beb5e5e20b1d3973c6a0d9a12c7d376a6ac
carolinarigaudfelix <carolinafelix.dev@gmail.com>
2023-05-05 17:58:49 -0300

    edit-works'

--- contacts/urls.py
+++ contacts/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.contact_list, name='contact_list'),
     path('<int:id_contact>', views.show_contact, name = 'show_contact'),
-    path('/edit/<int:id_contact>', views.edit_contact, name = 'edit_contact'),
+    path('edit/<int:id_contact>', views.edit_contact, name = 'edit_contact'),
+    path('new/', views.new_contact, name = 'new_contact'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
+from django.http import HttpResponse
 # Define o que vai acontecer quando o usuário faz algo, acessar uma rota por exemplo
 
 def contact_list(request):
@@ -13,11 +13,26 @@ def contact_list(request):
 
 def show_contact(request, id_contact):
     contact = get_object_or_404(Contact, id=id_contact)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name: # Verifica se o campo name está preenchido
+            contact.name = request.POST.get('name')
+            contact.email = request.POST.get('email')
+            contact.phone = request.POST.get('phone')
+            contact.save()
+            return redirect('show_contact', id_contact=id_contact)
+        else:
+            # Caso contrário, retorna uma mensagem de erro ou renderiza um template com uma mensagem de erro
+            return HttpResponse("O campo nome é obrigatório")
+    else:
+        {
+            'name': contact.name,
+            'email': contact.email,
+            'phone': contact.phone,
+            'category': contact.category,
+        }
+    return render(request, 'show_contact_new.html', {'contact': contact, 'id_contact': id_contact})
 
-    return render(request,
-                  "show_contact_new.html",
-                  {'contact': contact, 'id_contact': id_contact}
-                 )
 
 def edit_contact(request, id_contact):
     contact = get_object_or_404(Contact, id=id_contact)
@@ -26,6 +41,9 @@ def edit_contact(request, id_contact):
                   "edit_contact.html",
                   {'contact': contact, 'id_contact': id_contact}
             )
+
+def new_contact (request):
+     return render(request, "new_contact.html", {})
 
 def image_avatar(request):
     avatar = Contact.objects.all() # ou qualquer outro filtro
