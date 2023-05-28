f4310d0b51f37f3a08173b01f6224f4502e58a80
carolinarigaudfelix <carolrigaudfelix@gmail.com>
2023-05-28 20:06:16 -0300

    fixed-db and css

--- static/css/javascript/delete_contact.js
+++ static/css/javascript/delete_contact.js
@@ -2,6 +2,5 @@ function confirmDelete(event) {
     event.preventDefault(); 
     if (confirm("Are you sure you would like to delete this contact?")) 
         {document.getElementById("delete-form").submit() ;}
-        alert("Contact deleted successfully!");
 }    
 
